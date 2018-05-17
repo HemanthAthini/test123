e827cd991d66767ebcfbfe0f09ea00c6fd602fc8
HemanthAthini <38557802+HemanthAthini@users.noreply.github.com>
2018-05-17 12:31:40 +0530

    add this file branch

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+$arrayName = array("hello","world", "demo");
+
+echo $arrayName[0];
+echo $arrayName[1];
+echo $arrayName[2];
+
+echo "<br/>";
+
+echo "<pre>";print_r($arrayName);
+
+echo "<br/>";
+
+$arrayName2 = array("hemanth"=>26, "testName"=>27, )
+
+exit;
+
+
+ ?>
